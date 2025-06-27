ac0fc8196016b8533a2e15b5d4c7761dc74efc02
KarthikeyanD <karthiprasad2005@gmail.com>
2025-06-27 16:44:37 +0530

    footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AdminPage from "./Components/Blog/Pages/AdminPage";
 import PublicPage from "./Components/Blog/Pages/PublicPage";
 
